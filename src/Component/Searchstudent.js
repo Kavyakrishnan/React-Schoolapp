a7db05e124dc46d62313a298929fcf5683959a18
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-24 17:13:35 +0530

    complete

---
+++ src/Component/Searchstudent.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Searchstudent = () => {
+  return (
+    <div> <div class="Container">
+    <div class="row g-2">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-4">
+              
+                
+                
+               
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" class="form-label">Admission No </label>
+                    <input type="text" class="form-control"/>
+                </div>
+                
+              
+        
+           <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"> 
+            <button class="btn btn-success">Search</button>
+            </div>
+        </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default Searchstudent
